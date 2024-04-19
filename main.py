0601183618a0daa22e0d9d065e741e047f86c09a
starkgines <csilvagines@gmail.com>
2024-04-19 01:34:52 -0400

    added some funcionality

---
+++ main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from llama_index.experimental.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str
+
+load_dotenv()
+
+population_path = os.path.join("Data","population.csv")
+population_df = pd.read_csv(population_path)
+
+population_query_engine = PandasQueryEngine(df=population_df,verbose=True, instruction_str=instruction_str)
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+
+population_query_engine.query("What is the population of canada")
+
